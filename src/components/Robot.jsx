221c571bef79cc36c37feecc7b17ab6e993bf168
khushiiikk <khushiiikk@users.noreply.github.com>
2025-12-11 21:50:30 +0530

    Replace Chatbot CSS with new uploaded image logo

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -4,30 +4,10 @@ import './Robot.css';
 const Robot = ({ mood = 'happy', scale = 1, className = '' }) => {
     return (
         <div
-            className={`robot-container ${mood} ${className}`}
+            className={`robot-container image-mode ${mood} ${className}`}
             style={{ transform: `scale(${scale})` }}
         >
-            <div className="robot-head">
-                <div className="robot-eyes">
-                    <div className="eye left"></div>
-                    <div className="eye right"></div>
-                </div>
-                <div className="robot-antenna">
-                    <div className="antenna-ball"></div>
-                </div>
-            </div>
-            <div className="robot-body">
-                <div className="robot-logo">
-                    <div className="logo-circle">
-                        <span className="logo-text">M</span>
-                    </div>
-                </div>
-            </div>
-            <div className="robot-arms">
-                <div className="arm left"></div>
-                <div className="arm right"></div>
-            </div>
-            <div className="robot-shadow"></div>
+            <img src="/chatbot-logo.jpg" alt="Chatbot" className="robot-img" />
         </div>
     );
 };
