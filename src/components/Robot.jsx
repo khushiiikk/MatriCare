3175f35864629bcd50450dfeb3aaf89bf7312ecf
khushiiikk <khushiiikk@users.noreply.github.com>
2025-12-11 21:53:48 +0530

    Move chatbot logo to assets to ensure reliable bundling

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Robot.css';
+import chatbotLogo from '../assets/chatbot-logo.jpg';
 
 const Robot = ({ mood = 'happy', scale = 1, className = '' }) => {
     return (
@@ -7,7 +8,7 @@ const Robot = ({ mood = 'happy', scale = 1, className = '' }) => {
             className={`robot-container image-mode ${mood} ${className}`}
             style={{ transform: `scale(${scale})` }}
         >
-            <img src="/chatbot-logo.jpg" alt="Chatbot" className="robot-img" />
+            <img src={chatbotLogo} alt="Chatbot" className="robot-img" />
         </div>
     );
 };
